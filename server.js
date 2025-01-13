af1e3d7b531c7ffbb1e5ccec7842ac0624851639
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2025-01-13 11:15:58 +0530

    shift mongodb to online services

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -15,7 +18,7 @@ app.get('/', function (req, res) {
     res.send('Hi, there.');
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server is on');
 })
 
